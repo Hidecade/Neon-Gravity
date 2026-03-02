c60a359ed76972cac3514e4f8d21b609d2699a23
Hideki Konishi <69383319+Hidecade@users.noreply.github.com>
2026-03-03 07:45:30 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 // Game Configuration & Constants
 // =========================================================
 
-const GAME_VERSION = "1.0.4";
+const GAME_VERSION = "1.0.5";
 
 // --- システム設定 ---
 const G_SCALE = 0.7;          // 全体の描画スケール（標準の70%）
