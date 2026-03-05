84482808bce1b84809ba53e9d9f6749259a67a20
Hideki Konishi <69383319+Hidecade@users.noreply.github.com>
2026-03-05 19:34:52 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ★更新するたびに、ここのバージョン番号を書き換えてください（v1 -> v2 -> v3...）
-const CACHE_NAME = 'neon-gravity-v1.0.9';
+const CACHE_NAME = 'neon-gravity-v1.0.10';
 
 // キャッシュするファイルのリスト
 const ASSETS_TO_CACHE = [
@@ -70,6 +70,7 @@ self.addEventListener('fetch', (event) => {
         })
     );
 });
+
 
 
 
