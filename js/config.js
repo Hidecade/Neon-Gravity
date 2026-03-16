c6e6eb1c9829fe7c90f5d740d7416c9ce7361ecb
HIDEKI KONISHI <konishihideki@msn.com>
2026-03-16 10:38:43 +0900

    fix

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 // Game Configuration & Constants
 // =========================================================
 
-const GAME_VERSION = "1.3.14";
+const GAME_VERSION = "1.3.15";
 
 const DEBUG = {
     enabled: true,
