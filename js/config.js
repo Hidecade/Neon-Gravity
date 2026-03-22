416ccc20f11328d933d5a49d54b39ed179b492ef
HIDEKI KONISHI <konishihideki@msn.com>
2026-03-22 17:19:29 +0900

    v0.6.6

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 // Game Configuration & Constants
 // =========================================================
 
-window.GAME_VERSION = "0.6.5";
+window.GAME_VERSION = "0.6.6";
 
 
 const DEBUG = {
