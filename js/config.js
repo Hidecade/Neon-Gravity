f8de1f54712587b8e3809bdff1a86b0567168619
HIDEKI KONISHI <konishihideki@msn.com>
2026-03-22 18:03:23 +0900

    fix BGM_Stage 07

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 // Game Configuration & Constants
 // =========================================================
 
-window.GAME_VERSION = "0.6.6";
+window.GAME_VERSION = "0.6.7";
 
 
 const DEBUG = {
