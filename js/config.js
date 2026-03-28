18e5e624df4a38522b03c9f8145e580e6559f1a1
HIDEKI KONISHI <konishihideki@msn.com>
2026-03-28 17:28:44 +0900

    ｖ0.7.10

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 // Game Configuration & Constants
 // =========================================================
 
-window.GAME_VERSION = "0.7.9";
+window.GAME_VERSION = "0.7.10";
 
 
 const DEBUG = {
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ★更新するたびに、ここのバージョン番号を書き換えてください（v1 -> v2 -> v3...）
-const CACHE_NAME = 'neon-gravity-v0.7.9'; 
+const CACHE_NAME = 'neon-gravity-v0.7.10'; 
 
 // キャッシュするファイルのリスト
 const ASSETS_TO_CACHE = [
